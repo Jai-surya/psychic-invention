e5ee7d6126f3394c7f8d43aba5fc9a4c43ef0cc4
Jai Surya Pala <surya95.jsp@gmail.com>
2021-09-26 01:58:45 -0400

    Removing double Hello_World.py

--- py_games/Hello_World.py
+++ py_games/Hello_World.py
@@ -1,3 +1,16 @@
 from DT import *
 
 print("Hello World", now)
+
+
+
+from datetime import *
+
+now = datetime.now()
+dt_date = now.strftime("%d-%m-%Y")
+
+print("Hello World!\n", now)
+print("Today's Date is", dt_date)
+print("The time right now is", now.strftime("%H:%M:%S"))
+
+
