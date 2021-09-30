15953462d19528a80b631eeae1fa94b1b26d07d9
Jai Surya Pala <surya95.jsp@gmail.com>
2021-09-30 16:27:41 -0400

    Added option to specify output file

--- filescripts/filescan.py
+++ filescripts/filescan.py
@@ -2,18 +2,28 @@ import sys
 import os
 
 path = ''
-db_file = "file_names.txt"
+
+
+# db_file = "file_names.txt"
 
 
 # check the input command
 def check_arg():
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
         print('Script command format is :')
-        print('<python> <filename.py> <directory_path>')
+        print('<python> <filename.py> <directory_path> <out_file>')
         exit()
     else:
         global path
         path = sys.argv[1].replace('\'' '\"', '', 2)  # removes the '' and "" from the pass in arg
+
+
+def out_file():
+    if len(sys.argv) == 3:
+        global db_file
+        db_file = sys.argv[2]
+    else:
+        db_file = "file_names.txt"
 
 
 # read file names in path directory and store in file
@@ -33,6 +43,7 @@ def files(self):
 # main execution command
 def main():
     check_arg()
+    out_file()
     files(path)
 
 
